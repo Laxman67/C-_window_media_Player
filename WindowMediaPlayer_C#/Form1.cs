acb05e4904f5c8a8a2fc2addea75d0743edda9b0
laksh <laxmanhenricks94@Gmail.com.com>
2023-03-14 09:08:41 +0530

    Initial Commit of WMP

---
+++ WindowMediaPlayer_C#/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowMediaPlayer_C_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // To LOad Music
+
+            // Remeber to Add array and Several Vedios
+            string loc;
+            loc = "F:\\C#\\repos\\WindowMediaPlayer_C#\\Resources\\big_buck_bunny_720p_1mb.mp4";
+            this.axWindowsMediaPlayer1.URL = loc;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+    }
+}
